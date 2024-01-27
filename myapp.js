fb9070faa68a0c529526d9a18de427d3d2a7f359
AnasInno <130570546+AnasInno@users.noreply.github.com>
2024-01-27 22:25:56 +0000

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
+const path = require('path');
 
+
+app.use(express.static('public'));
+app.use(express.json());
 app.get('/', (req, res) => {
-  res.send('Hello Khadija!!')
-})
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/api/data', (req, res) => {
+  const dataReceived = req.body;
+  console.log('Data received:', dataReceived);
+  res.json({ message: 'Data received successfully!' });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
